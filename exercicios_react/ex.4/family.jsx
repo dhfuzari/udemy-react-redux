da4aa8145c546ba94aa95bd0575655bdb73d5ad9
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2017-12-27 21:52:08 -0200

    feat(family/member): componentes filhos

---
+++ exercicios_react/ex.4/family.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Member from './member'
+
+export default (props) => (
+    <div>
+        <Member name="ReactJS" lastName="Facebook" />
+        <Member name="AngularJS" lastName="Google" />
+        <Member name="MarkoJS" lastName="eBay" />
+    </div>
+)
