e5414dcf61c65457f3496050a65ca5243a73d95d
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2017-12-28 23:27:30 -0200

    feat(componentClasse): componente de classe

--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './ex.7/index.jsx',
+    entry: './ex.8/index.jsx',
     output: { 
         path: __dirname + '/public',
         filename: 'bundle.js'
