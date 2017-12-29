d23c3b234a09af7e599ee1c3069aa4b879424163
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2017-12-29 00:12:41 -0200

    feat(classComponent): componente com estado - parte 2

---
+++ exercicios_react/ex.10/field.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Field extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: this.props.initialValue }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {                
+        return(
+            <div>   
+                <label htmlFor="">{this.state.value}</label>
+                <input 
+                    type="text"
+                    onChange={this.handleChange}
+                    value={this.state.value}                    
+                />
+            </div>
+        );
+    }
+}
+
+export default Field;
