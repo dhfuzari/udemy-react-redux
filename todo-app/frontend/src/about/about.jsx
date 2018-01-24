6818ec512d5fa5b060c9965e84e319a981b76a72
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2018-01-24 00:24:17 -0200

    fix(pageHeader): page-header component

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa história</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque 
+            metus ligula, sagittis eget ullamcorper nec, mattis sed erat. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada 
+            fames ac turpis egestas. Donec laoreet, leo quis cursus hendrerit, 
+            psum dolor mollis felis, id condimentum nisi arcu et metus. Nunc 
+            consequat tristique ante ut commodo. Cras eu eros eget turpis 
+            consectetur malesuada. Proin quis nisi eu nisi rhoncus porttitor. 
+            Duis in pharetra nunc, at auctor eros. In hac habitasse platea 
+            dictumst. Ut egestas nunc non erat lobortis, quis gravida metus 
+            ultricies. Nam dapibus mi sed turpis tristique cursus.
+        </p>
+        <br/>
+
+        <h2>Missão e Visão</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque 
+            metus ligula, sagittis eget ullamcorper nec, mattis sed erat. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada 
+            fames ac turpis egestas. Donec laoreet, leo quis cursus hendrerit, 
+            psum dolor mollis felis, id condimentum nisi arcu et metus. Nunc 
+            consequat tristique ante ut commodo. Cras eu eros eget turpis 
+            consectetur malesuada. Proin quis nisi eu nisi rhoncus porttitor. 
+            Duis in pharetra nunc, at auctor eros. In hac habitasse platea 
+            dictumst. Ut egestas nunc non erat lobortis, quis gravida metus 
+            ultricies. Nam dapibus mi sed turpis tristique cursus.
+        </p>
+        <br/>
+
+        <h2>Valores</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque 
+            metus ligula, sagittis eget ullamcorper nec, mattis sed erat. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada 
+            fames ac turpis egestas. Donec laoreet, leo quis cursus hendrerit, 
+            psum dolor mollis felis, id condimentum nisi arcu et metus. Nunc 
+            consequat tristique ante ut commodo. Cras eu eros eget turpis 
+            consectetur malesuada. Proin quis nisi eu nisi rhoncus porttitor. 
+            Duis in pharetra nunc, at auctor eros. In hac habitasse platea 
+            dictumst. Ut egestas nunc non erat lobortis, quis gravida metus 
+            ultricies. Nam dapibus mi sed turpis tristique cursus.
+        </p>
+        <br/>
     </div>
 )
